04a0349ddf803b63256dd01a7383e2fb9e941857
Brb House on Fire <bobbywoj@sbcglobal.net>
2019-11-10 20:20:05 -0600

    commenting updates for readability

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,4 @@
-// const util = require('util');
-
-// This is our API key
+// API key
 const APIKey = "166a433c57516f51dfab1f7edaed8413";
 let lat = "";
 let lon = "";
@@ -15,11 +13,8 @@ let queryForecastURL = "https://api.openweathermap.org/data/2.5/forecast?" +
 let queryUVURL = "http://api.openweathermap.org/data/2.5/uvi/forecast?appid=" +
     APIKey + "&lat=" + lat + "&lon=" + lon;
     
-// 
+// set current date
 $("#cdate").text(moment().format("MMM Do YYYY"));
-
-
-
 
 let init = () => {
     makeCall("Chicago")
@@ -34,16 +29,16 @@ let makeCall = (city) => {
 }
 
 let callCurrent = (cityInput) => {
-    
+    // update the API URL
     queryCurrentURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput},us&units=imperial&appid=${APIKey}`;
-
+    // make the API call
     $.ajax({
         url: queryCurrentURL,
         method: "GET"
         }).then(function(response) {
             // Log the resulting object
-            console.log("Current:");
-            console.log(response);
+            // console.log("Current:");
+            // console.log(response);
     
             // Transfer content to HTML
             $("#currentCity").html(response.name);
@@ -52,9 +47,10 @@ let callCurrent = (cityInput) => {
             $("#ctemp").html("Temperature: " + response.main.temp + "&deg F");
             let img = response.weather[0].icon;
             $("#cimg").attr("src",`http://openweathermap.org/img/wn/${img}.png`)
-
+            // store latitude and longitude of the search city
             lat = response.coord.lat;
             lon = response.coord.lon;
+            // make new API call for UV index
             callUV(lat, lon);
 
 
@@ -69,22 +65,19 @@ let callForecast = (cityInput) => {
         method: "GET"
         }).then(function(response) {
             // Log the resulting object
-            console.log("Forecast:");
-            console.log(response);
-
+            // console.log("Forecast:");
+            // console.log(response);
+            // loop through the API results to pull values from each day
             for (var i = 0; i < 40; i+=8) {
                 let j = parseInt((i+9)/8);
                 // calculate future dates
                 let date = moment().add(j, 'days').format("MMM Do");
-                
                 let tag = `day${j}`
+                // display date
                 $(`#${tag}date`).text(date);
-    
                 // find and generate image icon
-                // console.log(response.list[i].weather[0].icon);
                 let img = response.list[i].weather[0].icon;
                 $(`#${tag}img`).attr("src",`http://openweathermap.org/img/wn/${img}.png`);
-    
                 // find and generate temperature
                 $(`#${tag}temp`).html(`${response.list[i].main.temp}&deg F`);
 
@@ -92,62 +85,67 @@ let callForecast = (cityInput) => {
     });
 }
 let callUV = (latty, lonny) => {
+    // update API url with key, longitude, and latitude
     queryUVURL = "http://api.openweathermap.org/data/2.5/uvi/forecast?appid=" +
     APIKey + "&lat=" + latty + "&lon=" + lonny;
+    // make the API call
     $.ajax({
         url: queryUVURL,
         method: "GET"
         }).then(function(response) {
             // Log the resulting object
-            console.log("UV:");
-            console.log(response);
-            // UV INDEX 
+            // console.log("UV:");
+            // console.log(response);
+            // display the result
             $("#cuv").text("UV Index: " + response[0].value);
     });
 }
 
-let storeCallData = () => {
-
-}
-
-let displayCurrentWeather = () => {
-    
-}
-
-let displayFiveDayForecast = () => {
-
-}
 
 let storeSearch = (cityInput) => {
+    // check if this is the initial page run before storing historical searches
     if (initKey !== 0) {
+        // pull history from local storage
         let string = localStorage.getItem("searchHistory");
+        // check if history exists
         if (string === null) {
+            // add new search to history array
             history.push(cityInput);
+            // store updated array into local storage
             localStorage.setItem("searchHistory", JSON.stringify(history));
         }
         else {
-            console.log("string: " + string);
+            // parse the string stored in local storage
             let object = JSON.parse(string);
+            // add the new search to the front of the array
             object.unshift(cityInput);
+            // save the updated info to the global array
             history = object;
+            // store the history array back in local storage
             localStorage.setItem("searchHistory", JSON.stringify(history));
         }
     }
 }
 
 let displayHistory = () => {
-    
+    // pull local storage for history
     let string = localStorage.getItem("searchHistory");
+    // if local storage is empty:
     if (string === null) {
         // do nothing
     }
     else {
+        // parse and store the array held in storage
         history = JSON.parse(string);
+        // for up to a max of 5 visible buttons
         for(var i = 1; i < 6; i++) {
+            // index marker
             j=i-1;
+            // check for empty or blank values in the array
             if(history[j] !== "" && history[j] !== null && history[j] !== undefined) {
-                console.log(history[j]);
+                // add the search text to a history button
                 $(`#city${i}`).text(history[j]);
+                // display the history button
                 $(`#city${i}`).removeClass("d-n");
             }
         }
@@ -155,24 +153,9 @@ let displayHistory = () => {
 
 }
 
-let clearPage = () => {
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
 $('#searchBox').keypress(function(e) {
     if (e.which == 13) {
+        // search on Enter-key
         $('#searchSubmit').click(); 
     }
 });
@@ -184,6 +167,7 @@ $('#searchSubmit').on('click', function() {
     // clear the searchbox
     $('#searchBox').val("");
     // console.log("User Search: " + userSearch);
+    // call the API to find out the weather
     makeCall(userSearch);
 });
 
@@ -195,6 +179,7 @@ $('.cityhistory').on('click', function() {
     // console.log("User Search: " + userSearch);
     // don't create a new history button when clicking a history item
     initKey = 0;
+    // call the API to find out the weather
     makeCall(userSearch);
 });
 
